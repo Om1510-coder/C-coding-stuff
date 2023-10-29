f3baa7a0f54eb5d8af2b63d7fd2f30b09b48f9b9
Om1510-coder <78219068+Om1510-coder@users.noreply.github.com>
2023-10-29 20:17:23 +0530

    Create hex_for_seven_segment.c

---
+++ hex_for_seven_segment.c
@@ -0,0 +1,45 @@
+print ('Hello World')
+while(1):
+    x = input()
+    if x == "A":
+        print("08H")
+    elif x == "B":
+        print("00H")
+    elif x == "C":
+        print("46H")
+    elif x == "D":
+        print("40H")
+    elif x == "E":
+        print("06H")
+    elif x == "F":
+        print("0DH")
+    elif x == "G":
+        print("02H")
+    elif x == "H":
+        print("09H")
+    elif x == "I":
+        print("4FH")
+    elif x == "J":
+        print("61H")
+    elif x == "K":
+        print("Sorry no K")
+    elif x == "L":
+        print("86H")
+    elif x == "M":
+        print("Sorry no M")
+    elif x == "N":
+        print("Sorry no N")
+    elif x == "O":
+        print("40H")
+    elif x == "P":
+        print("0CH")
+    elif x == "Q":
+        print("Sorry no Q")
+    elif x == "R":
+        print("08H")
+    elif x == "S":
+        print("12H")
+    elif x == "T":
+        print("")
+    elif x == "0":
+        exit()
