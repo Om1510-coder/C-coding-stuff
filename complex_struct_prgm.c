ce2a44b00196e8c3ec560330df846495201a5bb2
Om1510-coder <78219068+Om1510-coder@users.noreply.github.com>
2023-11-06 14:00:38 +0530

    Create complex_struct_prgm.c

---
+++ complex_struct_prgm.c
@@ -0,0 +1,33 @@
+//c code to take in two complex numbers and perform addition, subtraction, conjugate etc.
+
+#include <stdio.h>
+#include <math.h>
+struct complex_struct{
+    
+    int real_part;
+    int complex_part;
+}cs_a,cs_b,cs_sol;
+
+int main()
+{
+    //printf("Hello World");
+    printf("Complex number calculator\n");
+    printf("The first complex number: \n");
+    printf("Enter real part of first complex number: \n");
+    scanf("%d",&cs_a.real_part);
+    printf("Enter complex part of first complex number: \n");
+    scanf("%d",&cs_a.complex_part);
+    printf("The second complex number: \n");
+    printf("Enter real part of second complex number: \n");
+    scanf("%d",&cs_b.real_part);
+    printf("Enter complex part of second complex number: \n");
+    scanf("%d",&cs_b.complex_part);
+    printf("The addition of the complex numbers is: %d + %di",cs_a.real_part+cs_b.real_part,cs_a.complex_part+cs_b.complex_part);
+    printf("The subtraction of the complex numbers is: %d + %di",cs_a.real_part-cs_b.real_part,cs_a.complex_part-cs_b.complex_part);
+    printf("The conjugate of the first complex number is : %d %di\n",cs_a.real_part,(-1)*cs_a.complex_part);
+    printf("The conjugate of the second complex number is : %d %di\n",cs_b.real_part,(-1)*cs_b.complex_part);
+    printf("The magnitude of the first complex number is: %f\n",pow(((cs_a.real_part)*(cs_a.real_part))+((cs_a.complex_part)*(cs_a.complex_part)),0.5));
+    printf("The magnitude of the second complex number is: %f\n",pow(((cs_b.real_part)*(cs_b.real_part))+((cs_b.complex_part)*(cs_b.complex_part)),0.5));
+    return 0;
+}
+
