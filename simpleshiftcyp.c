574bfc99874598c57c13299dbc6ac55b64bf1fc8
Om1510-coder <78219068+Om1510-coder@users.noreply.github.com>
2023-11-15 14:34:12 +0530

    Update simpleshiftcyp.c

--- simpleshiftcyp.c
+++ simpleshiftcyp.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
@@ -8,18 +7,18 @@ char give_shift(char x,int y);
 int main()
 {
     //printf("Hello World");
-    char mystr[10];
-    char solstr[10];
+    char mystr[20];
+    char solstr[20];
     printf("Please enter the word to be shifted: ");
     scanf("%s",&mystr);
-    printf("The length of your word is: %d\n",strlen(mystr));
+    printf("The length of your word is: %ld\n",strlen(mystr));
     printf("Now enter the key for the shift cypher : ");
     int sk;
     scanf("%d",&sk);
     for(int i = 0;i<strlen(mystr);i++){
         solstr[i] = give_shift(mystr[i],sk);
     }
-    
+    printf("The shifted text is:\n");
     for(int j = 0;j<strlen(solstr);j++){
         printf("%c",solstr[j]);
     }
@@ -30,14 +29,41 @@ int main()
 
 char give_shift(char x,int y){
     int d;
+    char sol;
+    //printf("Please enter a charachter");
+    //scanf("%c",&x);
+    //printf("\nPlease enter key to shift");
+    //scanf("%d",&y);
     if(y>26){
         d = y/26;
         y = y - (26*d);
     }
+    if((x == '-') || (x == '_') || (x == '.')||(x == '!')||(x==' ')||(((int)x>=40)&&((int)x<=57))){
+        return x;
+    }
+    else{
     //printf("character value is : %c\n",x);
     //printf("ASCII value is : %d\n",x);
     //printf("ASCII value shifted : %d\n",x+y);
     //printf("character value is : %c\n",(int)x+y);
-    char sol =  (char)((int)x + y);
+    if(isupper(x)){
+        if(((int)x + y)>90){
+            sol = (char)(((int)x + y)- 26); 
+        }
+        else{
+            sol =  (char)((int)x + y);
+        }
+    }
+    else{
+        if(((int)x + y)>122){
+            sol = (char)(((int)x + y)- 26);
+        }
+        else{
+            sol = (char)((int)x + y);
+        }
+    }
+    }
+    //char sol =  (char)((int)x + y);
+    //printf("\a");
     return sol;
 }
